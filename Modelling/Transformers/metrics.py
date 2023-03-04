9b1392b9ad066edf3d97cd21958e5b12fbe60a8f
Hynek Kydlíček <kydliceh@aic.aic.hide.ms.mff.cuni.cz>
2023-03-04 17:04:30 +0100

    Transfomer baseline finisedh in lighting

--- Modelling/Transformers/metrics.py
+++ Modelling/Transformers/metrics.py
@@ -1,5 +1,9 @@
+from functools import partial
 from torchmetrics import Metric
 from dataclasses import dataclass
+from torchmetrics import Accuracy, ConfusionMatrix, F1Score, Precision
+from torchmetrics.classification import MulticlassConfusionMatrix
+from lightning.pytorch.loggers.wandb import WandbLogger
 
 
 @dataclass
@@ -25,3 +29,103 @@ class MetricWithMetadata(Metric):
     def reset(self):
         super().reset()
         self._metric.reset()
+
+
+
+def create_train_metrics(num_classes: int):
+    partial_create = partial(create_metric, num_classes=num_classes)
+    train_metrics = [
+        partial_create("accuracy", epoch=True, step=False),
+        partial_create("f1_macro", epoch=True, step=True),
+        partial_create("f1_micro", epoch=True, step=False),
+        partial_create("precision_macro", epoch=True, step=False),
+        partial_create("precision_micro", epoch=True, step=False),
+    ]
+    return train_metrics
+
+def create_test_metrics(num_classes: int):
+    partial_create = partial(create_metric, num_classes=num_classes)
+    test_metrics = [
+        partial_create("accuracy", epoch=True, step=False),
+        partial_create("f1_macro", epoch=True, step=False),
+        partial_create("f1_micro", epoch=True, step=False),
+        partial_create("precision_macro", epoch=True, step=False),
+    ]
+    return test_metrics
+
+def create_val_metrics(num_classes: int):
+    partial_create = partial(create_metric, num_classes=num_classes)
+    val_metrics = [
+        partial_create("accuracy", epoch=True, step=False),
+        partial_create("f1_macro", epoch=True, step=False),
+        partial_create("f1_micro", epoch=True, step=False),
+        partial_create("precision_macro", epoch=True, step=False),
+        partial_create("precision_micro", epoch=True, step=False),
+    ]
+    return val_metrics
+
+
+metric_defs = {
+    "accuracy": (Accuracy, {"task": "multiclass"}),
+    "f1_macro": (F1Score, {"task": "multiclass", "average": "macro"}),
+    "f1_micro": (F1Score, {"task": "multiclass", "average": "micro"}),
+    "precision_macro": (Precision, {"task": "multiclass", "average": "macro"}),
+    "precision_micro": (Precision, {"task": "multiclass", "average": "micro"}),
+    "confusion_matrix": (ConfusionMatrix, {"task": "multiclass"}),
+}
+
+def create_metric(metric_name, num_classes , epoch: bool, step: bool):
+    metric_def = metric_defs[metric_name]
+    metric = metric_def[0](**metric_def[1], num_classes=num_classes)
+    metadata = MetricMetadata(metric_name, epoch=epoch, step=step)
+    return MetricWithMetadata(metric, metadata)
+
+
+
+def log_confussion_matrix(model, cfs_matrix, split):
+    print(type(model.logger))
+    if not isinstance(model.logger, WandbLogger):
+        raise ValueError("Confusion matrix logging is only supported for wandb")
+
+    data, cols = prepare_confussion_matrix_for_logging(
+        cfs_matrix 
+    )
+    model.logger.log_table(
+        f"{split}/confusion_matrix_{model.current_epoch}", data=data, columns=cols
+    )
+
+def prepare_confussion_matrix_for_logging(confusion_matrix):
+    data = []
+    for i in range(confusion_matrix.shape[0]):
+        for j in range(confusion_matrix.shape[1]):
+            data.append((i, j, confusion_matrix[i, j]))
+    return data, ["Actual", "Predicted", "Count"]
+
+
+def log_metrics(model, predicted_labels, labels, split):
+        for metric in model.metrics[split + "_metrics"]:
+            if metric.metadata.step:
+                step_value = metric(predicted_labels, labels)
+                model.log(
+                    f"{split}/{metric.metadata.readable_name}_step",
+                    step_value,
+                    logger=True,
+                )
+            else:
+                metric.update(predicted_labels, labels)
+
+def reset_and_log_metrics(model, split):
+    for metric in model.metrics[split + "_metrics"]:
+        metric: MetricWithMetadata
+        if metric.metadata.epoch:
+            # Kinda monkey patched
+            if metric.real_type == MulticlassConfusionMatrix:
+                cm = metric.compute()
+                log_confussion_matrix(model, cm, split)
+            else:
+                model.log(
+                    f"{split}/{metric.metadata.readable_name}_epoch",
+                    metric.compute(),
+                    logger=True,
+                )
+        metric.reset()
--- Modelling/Transformers/train_transformer.py
+++ Modelling/Transformers/train_transformer.py
@@ -1,156 +1,54 @@
 from argparse import ArgumentParser
-from dataclasses import dataclass
-from typing import List, Tuple
-from torchmetrics import Accuracy, ConfusionMatrix, F1Score, Precision
 import wandb
-from lightning.pytorch.loggers.wandb import WandbLogger
-from lightning.pytorch.callbacks import (
-    LearningRateMonitor,
-    ModelCheckpoint,
-    Timer,
-    EarlyStopping,
-)
-from lightning.pytorch.loggers.wandb import WandbLogger
-from lightning.pytorch import Trainer
+from lightning.pytorch.cli import LightningCLI
 from lightning.pytorch.tuner.tuning import Tuner
-from pathlib import Path
 
-from metrics import MetricWithMetadata
-from metrics import MetricMetadata
-from CzechNewsDatamodule import NewsDataModule
-from FineTunedRobeCzech import FineTunedRobeCzech
-from callbacks import SimpleLayersFreezerCallback
+def fit(trainer, model, datamodule):
+    trainer.fit(model, datamodule)
 
+def validate(trainer, model, datamodule):
+    trainer.validate(model, datamodule)
 
-def create_metrics(class_names) -> Tuple[List[MetricWithMetadata], List[MetricWithMetadata], List[MetricWithMetadata]]:
-    metric_defs = {
-        "accuracy": (Accuracy, {"task": "multiclass", "num_classes": len(class_names)}),
-        "f1_macro": (F1Score, {"task": "multiclass", "num_classes": len(class_names), "average": "macro"}),
-        "f1_micro": (F1Score, {"task": "multiclass", "num_classes": len(class_names), "average": "micro"}),
-        "precision_macro": (Precision, {"task": "multiclass", "num_classes": len(class_names), "average": "macro"}),
-        "precision_micro": (Precision, {"task": "multiclass", "num_classes": len(class_names), "average": "micro"}),
-        "confusion_matrix": (ConfusionMatrix, {"task": "multiclass", "num_classes": len(class_names)}),
-    }
+def test(trainer, model, datamodule):
+    trainer.test(model, datamodule)
 
-    def create_metric(metric_name, epoch: bool, step: bool):
-        metric_def = metric_defs[metric_name]
-        metric = metric_def[0](**metric_def[1])
-        metadata = MetricMetadata(metric_name, epoch=epoch, step=step)
-        return MetricWithMetadata(metric, metadata)
+def tune(trainer, model, datamodule):
+    tuner = Tuner(trainer)
+    batch_size = tuner.scale_batch_size(model=model, datamodule=datamodule, mode="binsearch")
+    print(f"Found Batch size: {batch_size}")
 
-    train_metrics = [
-        create_metric("accuracy", epoch=True, step=False),
-        create_metric("f1_macro", epoch=True, step=True),
-        create_metric("f1_micro", epoch=True, step=False),
-        create_metric("precision_macro", epoch=True, step=False),
-        create_metric("precision_micro", epoch=True, step=False),
-    ]
-
-    test_metrics = [
-        create_metric("accuracy", epoch=True, step=False),
-        create_metric("f1_macro", epoch=True, step=False),
-        create_metric("f1_micro", epoch=True, step=False),
-        create_metric("precision_macro", epoch=True, step=False),
-        create_metric("precision_micro", epoch=True, step=False),
-    ]
-
-    val_metrics = [
-        create_metric("accuracy", epoch=True, step=False),
-        create_metric("f1_macro", epoch=True, step=False),
-        create_metric("f1_micro", epoch=True, step=False),
-        create_metric("precision_macro", epoch=True, step=False),
-        create_metric("precision_micro", epoch=True, step=False),
-        create_metric("confusion_matrix", epoch=True, step=False),
-    ]
-
-    return train_metrics, test_metrics, val_metrics
+    lr_finder = tuner.lr_find(model=model, datamodule=datamodule)
+    print(f"Found lr: {lr_finder.suggestion()}")
+    wandb.log({"lr_graph": lr_finder.plot(suggest=True)})
 
 
-def run(mode):
-    column = "server"
-    logger = WandbLogger(project=f"{column}_NN", log_model=True)
-    data_module = NewsDataModule(
-        column,
-        "ufal/robeczech-base",
-        max_length=512,
-        batch_size=64,
-        num_proc=1,
-        trunc_type="start",
-        cache_dir=Path("~/.cache/czech_news_proc").expanduser(),
-        reload_cache=False,
-    )
-    data_module.prepare_data()
-    data_module.setup()
-    class_names = data_module.train_dataset.features["labels"].names[1:]
-    train_metrics, test_metrics, val_metrics = create_metrics(class_names)
 
+class MyCli(LightningCLI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    model = FineTunedRobeCzech(
-        "ufal/robeczech-base",
-        class_names,
-        train_metrics=train_metrics,
-        test_metrics=test_metrics,
-        val_metrics=val_metrics,
-    )
-    directory = Path("logs") / f"{column}_NN" / str(logger.experiment.id)
-
-    trainer = Trainer(
-        max_epochs=4,
-        accelerator="gpu",
-        devices=4,
-        strategy="ddp",
-        callbacks=[
-            SimpleLayersFreezerCallback(2),
-            LearningRateMonitor(logging_interval="step"),
-            EarlyStopping(monitor="val/loss", patience=3, mode="min"),
-            ModelCheckpoint(
-                monitor="val/loss",
-                save_top_k=5,
-                mode="min",
-                dirpath=directory / "checkpoints",
-            ),
-            Timer(duration="02:00:00:00"),
-        ],
-        enable_model_summary=True,
-        enable_progress_bar=True,
-        logger=logger,
-        log_every_n_steps=1000,
-        val_check_interval=0.1,
-    )
-
-    match mode:
-        case "train":
-            trainer.fit(model, datamodule=data_module)
-            trainer.test(model, datamodule=data_module)
-
-        case "tune":
-            tuner = Tuner(trainer)
-            lr_result = tuner.lr_find(
-                model, datamodule=data_module, min_lr=1e-7, max_lr=1e-1, num_training=100
-            )
-            # lr
-            wandb.log({"lr_find_graph": lr_result.plot(suggest=True)})
-            print(f"Suggested learning rate: {lr_result.suggestion()}")
-
-
-            # batch size
-            batch_size_result = tuner.scale_batch_size(
-                model, datamodule=data_module
-            )
-            print(f"Suggested batch size: {batch_size_result}")
-
-        case "test":
-            trainer.test(model, datamodule=data_module)
-            
-
-def get_trainer_args():
-    parser = ArgumentParser()
-    parser.add_argument("mode", type=str, choices=["train", "tune", "test"])
-    args = parser.parse_args()
-    return args
-
+    def add_arguments_to_parser(self, parser: ArgumentParser):
+        parser.add_argument("--tune", action="store_true", default=False)
+        parser.add_argument("--fit", action="store_true", default=False)
+        parser.add_argument("--test", action="store_true", default=False)
+        parser.add_argument("--validate", action="store_true", default=False)
 
 
 if __name__ == "__main__":
-    args = get_trainer_args()
-    run(args.mode)
+    cli = MyCli(seed_everything_default=88, run=False, save_config_overwrite=True)
+    print(cli.trainer.num_devices)
+
+    if cli.config.tune == True:
+        tune(cli.trainer, cli.model, cli.datamodule)
+    
+    if cli.config.fit == True:
+        fit(cli.trainer, cli.model, cli.datamodule)
+
+    if cli.config.validate == True:
+        validate(cli.trainer, cli.model, cli.datamodule)
+
+    if cli.config.test == True:
+        test(cli.trainer, cli.model, cli.datamodule)
+
+
+
